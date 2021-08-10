6b4b59a9b8e8110f53f38da8732ff19eae608165
lexdos <shoaibnrr8@gmail.com>
2021-08-10 20:32:17 +0530

    Script to Check System Configuration

---
+++ System_Check.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+#Display the UID
+echo "UID: ${UID}"
+echo '==================================================================================='
+
+#Display the USERNAME of the Server
+USERNAME=`whoami`
+echo "USERNAME: ${USERNAME}"
+echo '==================================================================================='
+
+#Display the Hostname of the server
+echo "HOSTNAME: ${HOSTNAME}"
+echo '==================================================================================='
+
+#Display the OS running on the Server
+OS=` hostnamectl | sed -n '6p'`
+echo "${OS}"
+echo '==================================================================================='
+
+#Displaying the users
+USERS=`cat /etc/passwd | tail -n10 | awk -F ":" '{print $1}'`
+echo "Users: "
+echo "${USERS}"
+echo '==================================================================================='
+
+#Providing the User to choose 
+echo 'Enter 1 to Check the Ram ' 
+echo 'Enter 2 to Check the Disk Space Usage '
+echo 'Enter 3 to Check the CPU '
+echo 'Enter 4 to Check out  the Processes running on your System '
+
+read -p 'Enter the Choice as per your need: ' OPTION
+
+#Storing  the OUTPUT of free -h in RAM
+RAM=`free -h`
+
+#Storing  the OUTPUT of df -h in DISK
+DISK=`df -h`
+
+#To Check the Process 
+PROCESS=`ps -ef`
+case "${OPTION}" in 
+	"1")    echo
+		echo "Ram: "
+		echo "${RAM}"
+  	;;
+        "2")    echo
+                echo "Disk: "
+                echo "${DISK}"
+        ;;
+
+        "3")    echo 
+                echo "CPU: "
+		top
+        ;;
+        "4")    echo 
+                echo "CPU: "
+                echo "${PROCESS}"
+
+        ;;
+esac
